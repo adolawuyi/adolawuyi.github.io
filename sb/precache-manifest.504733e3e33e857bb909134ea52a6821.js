7aee6d2bcb610324294aebd33cea2ee6bfc0b81c
adolawuyi <adolawuyi@gmail.com>
2023-01-14 13:33:19 +0100

    build renamed

---
+++ sb/precache-manifest.504733e3e33e857bb909134ea52a6821.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "44a3f1c17f61e9885bdf",
+    "url": "/static/css/main.79ce4ec6.chunk.css"
+  },
+  {
+    "revision": "44a3f1c17f61e9885bdf",
+    "url": "/static/js/main.44a3f1c1.chunk.js"
+  },
+  {
+    "revision": "fa94f7c95b7c30b3d002",
+    "url": "/static/css/1.a5739f3c.chunk.css"
+  },
+  {
+    "revision": "fa94f7c95b7c30b3d002",
+    "url": "/static/js/1.fa94f7c9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "95769e6eb47a26eaf4a7b217f5c0d19f",
+    "url": "/static/media/amazon.95769e6e.mp4"
+  },
+  {
+    "revision": "2ef96d2b58521ce3a2b9655f4e861fd0",
+    "url": "/static/media/logo.2ef96d2b.png"
+  },
+  {
+    "revision": "6d83c4986bc074190da710d395dcd893",
+    "url": "/static/media/imghero.6d83c498.png"
+  },
+  {
+    "revision": "aaba630680c92c14c2cfe64ebe4ebf31",
+    "url": "/static/media/headerbg.aaba6306.png"
+  },
+  {
+    "revision": "4ffaaf602d5f4dacc9ab158736789c76",
+    "url": "/index.html"
+  }
+];
